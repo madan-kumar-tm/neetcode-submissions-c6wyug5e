2680b86a7708897053ac52bf2356695347822df4
MADAN KUMAR T M <madankumartm00@gmail.com>
2026-04-01 00:29:02 +0530

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       for(int i = 0; i < nums.length; i++){
+        int minIndex = i;
+        for(int j = i+1; j < nums.length; j++){
+            if(nums[minIndex] > nums[j]){
+                minIndex = j;
+            }
+        }    
+        int temp = nums[i];
+        nums[i] = nums[minIndex];
+        nums[minIndex] = temp;
+        
+       } 
+    }
+}
